e046c6d48dcacb4c4de85801916a61d1fb24f48e
Clancy Rowley <cwrowley@princeton.edu>
2015-07-04 13:56:27 -0400

    Implement subtraction

--- math_game.py
+++ math_game.py
@@ -6,7 +6,7 @@ from random import randint
 
 
 ntimes = 20
-subtraction = True
+subtraction = False
 lo = 1
 hi = 9
 score = 0
@@ -18,15 +18,22 @@ start = time()
 while (score < ntimes):
     a = randint(lo,hi)
     b = randint(lo,hi)
+    do_subtraction = subtraction and bool(randint(0,1))
+    if do_subtraction:
+        question = "%d - %d = " % (a + b, b)
+        correct_answer = a
+    else:
+        question = "%d + %d = " % (a, b)
+        correct_answer = a + b
     while True:
         try:
-            ans = int(raw_input("%2d) %d + %d = " % (score + 1, a, b)))
+            ans = int(raw_input("%2d) " % (score + 1) + question))
             break
         except ValueError:
             print("Invalid input.  Please enter a number.")
-    if ans == a + b:
+    if ans == correct_answer:
         score += 1
     else:
-        print("Oops.  The correct answer is %d." % (a + b))
+        print("Oops.  The correct answer is %d." % correct_answer)
 end = time()
 print("Total time: %.2f seconds" % (end - start))
